801b8065bfbeded0dc485bdd231db8c7fc9edc68
kyle <oliverke@mail.uc.edu>
2022-08-06 19:55:24 -0400

    Adds path regex

--- url_simple/components.py
+++ url_simple/components.py
@@ -11,6 +11,8 @@ from url_simple.constants import (
     sub_delims,
     reserved,
     unreserved,
+    pchar,
+    reg_name,
 )
 from url_simple.exceptions import (
     ValidationError,
@@ -59,10 +61,12 @@ class Scheme(URIComponent):
     value_regex: re.Pattern = re.compile(r'(?P<scheme>[a-zA-Z][a-zA-Z\d+.-]+)')
     validation_error = InvalidSchemeError
 
+
 class UserInfo(URIComponent):
     suffix: str = '@'
     value_regex: re.Pattern = re.compile(rf'(?P<user_info>(?:{unreserved}|{pct_encoded}|{sub_delims}|:)*)')
     validation_error = InvalidUserInfoError
+
 
 class Hostname(URIComponent):
     label_regex: re.Pattern = re.compile(r'[a-zA-Z\d-]{1,63}')
@@ -72,15 +76,18 @@ class Hostname(URIComponent):
     )
     validation_error = InvalidHostnameError
 
+
 class IPV4(URIComponent):
     value_regex: re.Pattern = re.compile(r'(?P<ipv4>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
     validation_error = InvalidIPV4Error
+
 
 class IPV6(URIComponent):
     prefix: str = '['
     suffix: str = ']'
     value_regex: re.Pattern = re.compile(rf'(?P<ipv6>{hex_digit.pattern}{{1,4}}(?::{hex_digit.pattern}{{1,4}}){7})')
     validation_error = InvalidIPV6Error
+
 
 class Host(URIComponent):
     value_regex: re.Pattern = re.compile(rf'(?P<host>{Hostname.as_pattern()}|{IPV4.as_pattern()}|{IPV6.as_pattern()}')
@@ -91,10 +98,12 @@ class Host(URIComponent):
         self.ipv4: IPV4 | None = IPV4(value=match.group('ipv4'))
         self.ipv6: IPV6 | None = IPV6(value=match.group('ipv6'))
 
+
 class Port(URIComponent):
     prefix: str = ':'
     value_regex: re.Pattern = re.compile(r'(?P<port>\d{1,5})')
     validation_error = InvalidPortError
+
 
 class Authority(URIComponent):
     prefix: str = '//'
@@ -108,13 +117,25 @@ class Authority(URIComponent):
 
 
 class Path(URIComponent):
-    value_regex: re.Pattern = re.compile(r'^(?P<path>.)')
+    pchar: re.Pattern = re.compile(rf'{unreserved}|{pct_encoded}|{sub_delims}|[:@]')
+    segment: re.Pattern = re.compile(rf'{pchar}*')
+    segment_nz: re.Pattern = re.compile(rf'{pchar}+')
+    segment_nz_nc: re.Pattern = re.compile(rf'(?:{unreserved}|{pct_encoded}|{sub_delims}|@)+')
+
+    abempty: re.Pattern = re.compile(rf'(/{segment})*')
+    absolute: re.Pattern = re.compile(rf'/(?:{segment_nz}(?:/{segment})*)?')
+    noscheme: re.Pattern = re.compile(rf'{segment_nz_nc}(?:/{segment})*')
+    rootless: re.Pattern = re.compile(rf'{segment_nz}(?:/{segment})*')
+    empty: re.Pattern = re.compile(r'^$')
+
+    value_regex: re.Pattern = re.compile(rf'(?P<path>{abempty}|{absolute}|{noscheme}|{rootless}|{empty})')
     validation_error = InvalidPathError
 
 
 class Query(URIComponent):
     value_regex: re.Pattern = re.compile(r'^(?P<query>.)')
     validation_error = InvalidQueryError
+
 
 class Fragment(URIComponent):
     value_regex: re.Pattern = re.compile(r'^(?P<fragment>.)')
